09a1fb12fc604cb1d9083ff1f6fe367774d9978b
M.A. Parks, PhD (they/them) <maxaeonparks@gmail.com>
2025-05-18 01:21:41 -0400

    Populate sketch.js

--- duck-rabbit-philosophy-game/js/sketch.js
+++ duck-rabbit-philosophy-game/js/sketch.js
@@ -1,1 +1,35 @@
-// Placeholder
+let currentScene = 'farmMap';
+let duck, rabbit, duckRabbitIcon;
+
+function preload() {
+  duck = new Character('duck');
+  rabbit = new Character('rabbit');
+  duckRabbitIcon = loadImage('assets/images/icons/duck-rabbit.png');
+  preloadScenes();
+  preloadLetters();
+}
+
+function setup() {
+  createCanvas(800, 600);
+  setupScenes();
+}
+
+function draw() {
+  background(220);
+  drawScene(currentScene);
+  duck.display();
+  rabbit.display();
+  image(duckRabbitIcon, width - 70, 10, 60, 60);
+}
+
+function mousePressed() {
+  handleSceneClicks(mouseX, mouseY);
+  handleLetterClicks(mouseX, mouseY);
+  if (mouseX > width - 70 && mouseY < 70) {
+    showAdvice();
+  }
+}
+
+function showAdvice() {
+  alert("Duck-Rabbit says: Think about things from a different perspective!");
+}
